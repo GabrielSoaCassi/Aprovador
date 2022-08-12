3b2f9383df908527c9585d875e2e761cdd8b736b
Gabriel Soares Cassimiro <gabrielcassimiro4@gmail.com>
2022-08-12 20:58:18 -0300

    alterado formulario em escritorio tanto cadastro quando edicao

--- front-end/src/app/models/escritorio/cadastrar-escritorio/cadastrar-escritorio.component.ts
+++ front-end/src/app/models/escritorio/cadastrar-escritorio/cadastrar-escritorio.component.ts
@@ -1,23 +1,38 @@
 import { EscritorioService } from './../escritorio.service';
 import { Escritorio } from './../../interface/Escritorio';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-cadastrar-escritorio',
   templateUrl: './cadastrar-escritorio.component.html',
 })
 export class CadastrarEscritorioComponent {
   Escritorio!: Escritorio;
-  nome!: string;
-
-  constructor(private escritorioService: EscritorioService) { }
+  escritorioForm: FormGroup;
+  sucessoPost: boolean = false;
+  erroPost: boolean = true;
+  constructor(
+    private escritorioService: EscritorioService,
+    private fb: FormBuilder
+  ) {
+    this.escritorioForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
 
   cadastrarEscritorio(): void {
-    if (this.nome == null || this.nome == undefined || this.nome == "")
-      throw new Error()
-    this.Escritorio = { nome: this.nome };
+    this.Escritorio = { nome: this.escritorioForm.controls.nome.value };
     if (this.Escritorio.nome != null || this.Escritorio.nome != undefined) {
-      this.escritorioService.cadastrarEscritorios(this.Escritorio).subscribe(() => alert('Escritório cadastrado com sucesso!'));
-      this.nome = "";
+      this.escritorioService.cadastrarEscritorios(this.Escritorio).subscribe(
+        () => { this.sucessoPost = true},
+        (err: HttpErrorResponse) => (this.erroPost = err.ok)
+      );
+      this.escritorioForm.reset();
     }
+  }
+  fecharAlerta(): void {
+    this.erroPost = true;
+    this.sucessoPost = false;
   }
 }
--- front-end/src/app/models/escritorio/editar-escritorio/editar-escritorio.component.ts
+++ front-end/src/app/models/escritorio/editar-escritorio/editar-escritorio.component.ts
@@ -2,37 +2,56 @@ import { EscritorioService } from './../escritorio.service';
 import { Component, OnInit } from '@angular/core';
 import { Escritorio } from '../../interface/Escritorio';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-editar-escritorio',
   templateUrl: './editar-escritorio.component.html',
 })
 export class EditarEscritorioComponent implements OnInit {
-  escritorio: Escritorio;
-  escritorioNovo: Escritorio;
   escritorioId: number;
-  nome: string;
+  sucessoPost: boolean = false;
+  erroPost: boolean = true;
+  escritorioForm: FormGroup;
 
-  constructor(private escritorioService: EscritorioService,
-    private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private escritorioService: EscritorioService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {
+    this.escritorioForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.min(5)]],
+    });
+  }
 
   ngOnInit(): void {
     this.trazerEscritorioParaEditar();
   }
 
   private trazerEscritorioParaEditar() {
-    let escritorioId: number;
-    this.activatedRoute.params.subscribe(params => escritorioId = params.id);
+    this.activatedRoute.params.subscribe(
+      (params) => (this.escritorioId = params.id)
+    );
     this.escritorioService
-      .listarEscritorioPorId(escritorioId)
-      .subscribe(escritorio => this.escritorio = escritorio);
+      .listarEscritorioPorId(this.escritorioId)
+      .subscribe((escritorio) =>
+        this.escritorioForm.patchValue({ ...escritorio })
+      );
   }
 
   editarEscritorio(): void {
-    if (this.escritorio.id <= 0 || this.nome == null || this.nome == undefined)
-      throw new Error();
-    this.escritorioNovo ={ id: this.escritorio.id,nome: this.nome }
-    this.escritorioService.atualizarEscritorio(this.escritorioNovo).subscribe(() => alert("Escritório atualizado!!"))
-    this.nome = "";
+    let escritorioNovo: Escritorio = {
+      id: this.escritorioId,
+      nome: this.escritorioForm.controls.nome.value,
+    };
+    this.escritorioService
+      .atualizarEscritorio(escritorioNovo)
+      .subscribe(() => this.sucessoPost = true , (err:HttpErrorResponse) => this.erroPost = err.ok);
+  }
+
+  fecharAlerta(): void {
+    this.erroPost = true;
+    this.sucessoPost = false;
   }
 }
